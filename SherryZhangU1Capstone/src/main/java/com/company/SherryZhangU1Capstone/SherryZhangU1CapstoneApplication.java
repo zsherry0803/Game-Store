81c3e9da0005cf8add1d6ca849906a79463f5a68
sherry <zsherry0803@gmail.com>
2019-07-11 10:33:16 -0400

    proj initialized

---
+++ SherryZhangU1Capstone/src/main/java/com/company/SherryZhangU1Capstone/SherryZhangU1CapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.company.SherryZhangU1Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SherryZhangU1CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SherryZhangU1CapstoneApplication.class, args);
+	}
+
+}
